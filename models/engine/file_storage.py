a5eced24715ec80da857b97a2c6fc92fa87cb3a0
Drew-24 <andrewwil1@hotmail.com>
2022-04-11 12:48:06 -0400

    Delete object

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,15 +2,24 @@
 """This module defines a class to manage file storage for hbnb clone"""
 import json
 
+from requests import delete
+
 
 class FileStorage:
     """This class manages storage of hbnb models in JSON format"""
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if (cls is None):
+            return FileStorage.__objects
+        else:
+            dictionary = {}
+            for key, value in FileStorage.__objects.items():
+                if value.__class__ == cls:
+                    dictionary[key] = value
+            return dictionary
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -36,15 +45,23 @@ class FileStorage:
         from models.review import Review
 
         classes = {
-                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
-                    'State': State, 'City': City, 'Amenity': Amenity,
-                    'Review': Review
-                  }
+            'BaseModel': BaseModel, 'User': User, 'Place': Place,
+            'State': State, 'City': City, 'Amenity': Amenity,
+            'Review': Review
+        }
         try:
             temp = {}
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        """ deletes objects from __objects if it's inside """
+        if (obj is None):
+            return
+        key = obj.to_dict()['__class__'] + '.' + obj.id
+        if key in FileStorage.__objects:
+            del FileStorage.__objects[key]

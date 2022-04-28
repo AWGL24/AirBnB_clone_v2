83e8bc3a29ef97388d4bd7a843578e178279543f
Drew-24 <andrewwil1@hotmail.com>
2022-04-28 11:32:16 -0400

    Fixed code

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -8,11 +8,11 @@ sudo apt-get -y install nginx
 sudo ufw allow 'Nginx HTTP'
 
 # create folders if they don't already exist
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
 
 # created fake html file
-echo "
+sudo echo "
 <!DOCTYPE html>
 <html>
 <head>
@@ -23,13 +23,13 @@ Holberton School
 </html> " | sudo tee /data/web_static/releases/test/index.html
 
 # created symlink
-ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 # Gived ownership of data folder to ubuntu user and group
 sudo chown -hR ubuntu:ubuntu /data/
 
 # Updates rge Nginx config to serve the content
-sed -i "40 i \n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-available/default
+sudo sed -i "40 i \n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-available/default
 
 # restarts nginx for the update to take effect
 sudo service nginx restart

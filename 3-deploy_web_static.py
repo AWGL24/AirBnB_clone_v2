94515546807df923cd13e21f247bafd96a9e43ab
Drew-24 <andrewwil1@hotmail.com>
2022-05-12 13:26:08 -0400

    Codigo malo needs fixing

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
-''' Module holds a Fabric script (based on the file 2-do_deploy_web_static.py)
+''' Write a Fabric script (based on the file 2-do_deploy_web_static.py)
 that creates and distributes an archive to your web servers,
 using the function deploy '''
 
-from fabric.api import env, local, put, run
+import os
+from os import path
+from os.path import isdir
+from time import strftime
+from fabric.api import local
 from datetime import datetime
-from os.path import exists, isdir
-
-env.hosts = ["34.139.253.47", "107.21.145.67"]
+from fabric.api import run, put, env
 
 
 def do_pack():
-    ''' do_pack function '''
+    """ do_pack function """
     try:
         date = datetime.now().strftime("%Y%m%d%H%M%S")
         if isdir("versions") is False:
@@ -23,30 +25,34 @@ def do_pack():
         return None
 
 
+env.hosts = ["34.139.253.47", "107.21.145.67"]
+
+
 def do_deploy(archive_path):
-    ''' do_deploy function '''
-    if exists(archive_path) is False:
-        return False
-    try:
-        file = archive_path.split("/")[-1]
-        no_ext = file.split(".")[0]
-        path = "/data/web_static/releases/"
-        put(archive_path, '/tmp/')
-        run('mkdir -p {}{}/'.format(path, no_ext))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(file, path, no_ext))
-        run('rm /tmp/{}'.format(file))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
-        run('rm -rf {}{}/web_static'.format(path, no_ext))
-        run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
-        return True
-    except:
+    """ do_deploy function """
+    if path.exists(archive_path):
+        try:
+            put(archive_path, '/tmp/')
+            filename = archive_path[9:]
+            no_ext = filename[:-4]
+            dir_name = '/data/web_static/releases/' + no_ext + '/'
+            run('mkdir -p ' + dir_name)
+            run('tar -xzf /tmp/' + filename + ' -C ' + dir_name)
+            run('rm -f /tmp/' + filename)
+            run('mv ' + dir_name + '/web_static/* ' + dir_name)
+            run('rm -rf /data/web_static/current')
+            run('ln -s ' + dir_name + ' /data/web_static/current')
+            print('New version deployed!')
+            return True
+        except:
+            return False
+    else:
         return False
 
 
 def deploy():
     ''' deploy function '''
     archive_path = do_pack()
-    if archive_path is None:
+    if path is None:
         return False
     return do_deploy(archive_path)
